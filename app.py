27fe202594986c5cd637a59b1614ace7795a2b89
Таир Пятислямов <tair@MacBook-Air-Tair.local>
2021-11-06 01:02:21 +0300

    Initialize Git repository

--- app.py
+++ app.py
@@ -1,23 +1,20 @@
 from flask import Flask, request, render_template, jsonify, url_for
-from utils import clean_text
+# from utils import clean_text
 import pickle
 import time
 import os
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.svm import LinearSVC
+
 
 app = Flask(__name__)
 
-MODEL_VERSION = 'model_V0.pkl'
-VECTORIZER_VERSION = 'vectorizer_V0.pkl'
 
-# load model assets
-vectorizer_path = os.path.join(os.getcwd(), 'model_assets', VECTORIZER_VERSION)
-model_path = os.path.join(os.getcwd(), 'model_assets', MODEL_VERSION)
-vectorizer = pickle.load(open(vectorizer_path, 'rb'))
-model = pickle.load(open(model_path, 'rb'))
 
 # TODO: add versioning to url
 @app.route('/', methods=['GET', 'POST'])
 def predict():
+    # count_vect = CountVectorizer()
     """ Main webpage with user input through form and prediction displayed
 
     :return: main webpage host, displays prediction if user submitted in text field
@@ -25,35 +22,43 @@ def predict():
 
     if request.method == 'POST':
 
-        response = request.form['text']
-        input_text = clean_text(response)
-        input_text = vectorizer.transform([input_text])
-        prediction = model.predict(input_text)
-        prediction = 'Cyber-Troll' if prediction[0] == 1 else 'Non Cyber-Troll'
-        return render_template('index.html', text=prediction, submission=response)
+        text = request.form['text']
+        # input_text = clean_text(response)
+        # input_text = vectorizer.transform([input_text])
+        # prediction = model.predict(input_text)
+        # prediction = 'Cyber-Troll' if prediction[0] == 1 else 'Non Cyber-Troll'
+        with open('vectorizer', 'rb') as vec:
+            count_vect = pickle.load(vec)
+        with open('multiclass_classifier_rf', 'rb') as training_model:
+            model = pickle.load(training_model)
+        print(model.predict(count_vect.transform(['Ищу таргетолога. Рассматриваю начинающих специалистов тоже. Писать в лс'])))
+        response = model.predict(count_vect.transform([str(text)]))
+
+        return render_template('index.html', text=text, submission=str(response[0]))
 
     if request.method == 'GET':
         return render_template('index.html')
 
 # TODO: add versioning to api
-@app.route('/predict', methods=['POST'])
-def predict_api():
-    """ endpoint for model queries (non gui)
+# @app.route('/predict', methods=['POST'])
+# def predict_api():
+#     """ endpoint for model queries (non gui)
 
-    :return: json, model prediction and response time
-    """
-    start_time = time.time()
+#     :return: json, model prediction and response time
+#     """
+#     start_time = time.time()
 
-    request_data = request.json
-    input_text = request_data['data']
-    input_text = clean_text(input_text)
-    input_text = vectorizer.transform([input_text])
-    prediction = model.predict(input_text)
-    prediction = 'Cyber-Troll' if prediction[0] == 1 else "Non Cyber-Troll"  # post processing
+#     # request_data = request.json
+#     # input_text = request_data['data']
+#     # input_text = clean_text(input_text)
+#     # input_text = vectorizer.transform([input_text])
+#     # prediction = model.predict(input_text)
+#     # prediction = 'Cyber-Troll' if prediction[0] == 1 else "Non Cyber-Troll"  # post processing
 
-    response = {'prediction': prediction, 'response_time': time.time() - start_time}
-    return jsonify(response)
+#     response = {'prediction': 'prediction', 'response_time': time.time() - start_time}
+#     print(response)
+#     return jsonify(response)
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
